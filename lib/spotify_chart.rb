8a497e971d1baac2b96f3e03acb49be08b9d78a7
Julia <juliazhang86@gmail.com>
2014-11-02 16:17:57 -0500

    finsihed

--- lib/spotify_chart.rb
+++ lib/spotify_chart.rb
@@ -1,24 +1,33 @@
 require 'json'
 require 'open-uri'
+require 'pry'
 
 class SpotifyChart
+
 
   attr_reader :base_url
 
   def initialize
-    @base_url = "?"
+    @base_url = "http://charts.spotify.com/api/charts/"
   end
 
   def get_url(preference, region)
+    "#{@base_url}#{preference}/#{region}/latest"
     # return a string that is the base url + / + preference + / + region + / + latest
   end
 
   def get_json(url)
+    JSON.load(open(url))
     # load json given a url here
     # refer to the references if you have questions about this
   end
 
   def fetch_track_album_artist(music_hash)
+    first_track = music_hash["tracks"][0]
+
+    "'#{first_track["track_name"]}' by #{first_track["artist_name"]} from the album #{first_track["album_name"]}"
+
+
     # example music_hash:
     # {  
     #   "tracks" => [  
@@ -46,23 +55,36 @@ class SpotifyChart
 
 
   def most_streamed(region)
-    # call no get_url here, where preference is the string 'most_streamed',
+
+    most_streamed_url = get_url('most_streamed', region)
+    # call on get_url here, where preference is the string 'most_streamed',
     # and set it equal to a variable
+
+    most_streamed_json = get_json(most_streamed_url)
 
     # call on get_json here, using the string that get_url returns
     
+    fetch_track_album_artist(most_streamed_json)
+
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
   end
 
   def most_shared(region)
-    # call no get_url here, where preference is the string 'most_shared',
+    # call on get_url here, where preference is the string 'most_shared',
     # and set it equal to a variable
 
+    most_streamed_url = get_url('most_shared', region)
+
     # call on get_json here, using the string that get_url returns
+
+    most_streamed_json = get_json(most_streamed_url)
     
     # finally, call on fetch_track_album_artist using the 
     # hash that get_json returns
+
+    fetch_track_album_artist(most_streamed_json)
   end
 
 end
+
